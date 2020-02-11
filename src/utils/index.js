4dd98d14fcd4d4c30281140d8161603f8f45b189
Jan Krakora <jan.krakora@diribet.cz>
2020-02-11 14:51:49 +0100

    use window.postMessage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,19 @@
 export function createContent(element, properties) {
     if (element == null) {
-        throw new Error("You have to provide a DOM element where to load the embeddable content");
+        throw new Error('You have to provide a DOM element where to load the embeddable content');
     }
 
     const iframe = document.createElement('iframe');
     iframe.src = properties.url;
-    iframe.className = 'chy-embeddable-frame loading';
+    iframe.className = 'chy-embeddable-frame';
+    iframe.style.margin = '0';
+    iframe.style.padding = '0';
     iframe.style.border = 'none';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
 
     iframe.addEventListener('load', function() {
-        console.info('load');
-        iframe.classList.remove('loading');
+        iframe.contentWindow.postMessage(properties, '*');
     });
 
     element.appendChild(iframe);
