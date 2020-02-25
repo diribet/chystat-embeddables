a4bf69510e0f5318539e030eee2944f3b83333e0
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2020-02-25 17:05:16 +0100

    embeddable initialization use messages instead of onLoad

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import {createContent} from "./utils";
-
 const EMBEDDABLE_PATH = '/embeddable';
+
+let embeddableIdSequence = 0;
 
 function graphic(element, properties) {
     properties.url += EMBEDDABLE_PATH;
     properties.type = 'graphic';
 
     createContent(element, properties);
+}
+
+function createContent(element, properties) {
+    if (element == null) {
+        throw new Error('You have to provide a DOM element where to load the embeddable content');
+    }
+
+    const embeddableId = embeddableIdSequence;
+    embeddableIdSequence++;
+    const iframeUrl = properties.url + "#id=" + embeddableId;
+
+    const iframe = document.createElement('iframe');
+    iframe.src = iframeUrl;
+    iframe.className = 'chy-embeddable-frame';
+    iframe.style.margin = '0';
+    iframe.style.padding = '0';
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+
+    window.addEventListener('message', function(event) {
+        if (event.data.type === "embeddable-loaded" && event.data.embeddableId == embeddableId) {
+            const message = {
+                type: "embeddable-settings",
+                settings: properties
+            };
+            iframe.contentWindow.postMessage(message, '*');
+        }
+    });
+
+    element.appendChild(iframe);
 }
 
 export {
